0d124f470d5d3cf4052d89e9336dc224b51c5d97
Rodrigo Moutinho <rcm1989@gmail.com>
2019-07-13 09:09:34 -0300

    feat(core): 🎸 new scope configurations
    Now it's possible to select project's scopes during the commit process.

--- changelog.config.js
+++ changelog.config.js
@@ -22,7 +22,11 @@ module.exports = {
         "issues",
         "lerna"
     ],
-    "scopes": [],
+    "scopes": [
+        "",
+        "README",
+        "core"
+    ],
     "types": {
         "chore": {
             "description": "Build process or auxiliary tool changes",
